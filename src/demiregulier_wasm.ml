bfcb20e324852939d94529962a31905ecd4ea784
John Samuel <johnsamuelwrites@gmail.com>
2026-04-04 12:46:28 +0200

    Handle [3³· 4² ; 3⁶] triangles

--- src/demiregulier_wasm.ml
+++ src/demiregulier_wasm.ml
@@ -860,9 +860,8 @@ def _gen_bi_elongtri_a(larg, haut, a):
             _ajouter_tuile_4_direct(x, y, x + a, y, x + a, y + a, x, y + a, larg, haut)
             _ajouter_tuile_3_direct(x, y + a, x + a / 2.0, y + a + h, x + a, y + a, larg, haut)
             _ajouter_tuile_3_direct(x, y + a + 2.0 * h, x + a / 2.0, y + a + h, x + a, y + a + 2.0 * h, larg, haut)
-            _ajouter_tuile_4_direct(x, y + a + 2.0 * h, x + a, y + a + 2.0 * h, x + a, y + 2.0 * a + 2.0 * h, x, y + 2.0 * a + 2.0 * h, larg, haut)
             x = x + a
-        y = y + periode_y + a
+        y = y + periode_y
     retour 0
 
 
@@ -878,9 +877,8 @@ def _gen_bi_elongtri_b(larg, haut, a):
             _ajouter_tuile_3_direct(x, y + a, x + a / 2.0, y + a + h, x + a, y + a, larg, haut)
             _ajouter_tuile_3_direct(x, y + a + h, x + a / 2.0, y + a + 2.0 * h, x + a, y + a + h, larg, haut)
             _ajouter_tuile_3_direct(x, y + a + 2.0 * h, x + a / 2.0, y + a + 3.0 * h, x + a, y + a + 2.0 * h, larg, haut)
-            _ajouter_tuile_4_direct(x, y + a + 3.0 * h, x + a, y + a + 3.0 * h, x + a, y + 2.0 * a + 3.0 * h, x, y + 2.0 * a + 3.0 * h, larg, haut)
             x = x + a
-        y = y + periode_y + a
+        y = y + periode_y
     retour 0
 
 
